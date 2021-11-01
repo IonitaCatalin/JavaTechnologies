d0940a2f3be9dc7816d6615d1c988e75bd191465
IonitaCatalin <ionita.catalin2000@gmail.com>
2021-11-01 06:33:27 +0200

    some more work,far from being finished

---
+++ Labs4/src/main/java/com/jtechnologies/labs4/beans/EnrolmentInputBean.java
@@ -0,0 +1,42 @@
+package com.jtechnologies.labs4.beans;
+
+import com.jtechnologies.labs4.dao.EnrolmentDAO;
+import com.jtechnologies.labs4.dao.EnrolmentDAOImpl;
+import com.jtechnologies.labs4.models.Enrolment;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "EnrolmentInputBean", eager = false)
+@RequestScoped
+public class EnrolmentInputBean {
+
+    private int studentId;
+    private int examId;
+
+    private EnrolmentDAO enrolmentDAO;
+
+    public EnrolmentInputBean() {
+        enrolmentDAO = new EnrolmentDAOImpl();
+    }
+
+    public void submit() {
+        enrolmentDAO.addEnrolment(new Enrolment(studentId,examId));
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getExamId() {
+        return examId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setExamId(int examId) {
+        this.examId = examId;
+    }
+}
---
+++ Labs4/src/main/java/com/jtechnologies/labs4/beans/StudentBean.java
@@ -0,0 +1,43 @@
+package com.jtechnologies.labs4.beans;
+
+import com.jtechnologies.labs4.dao.StudentDAO;
+import com.jtechnologies.labs4.dao.StudentDAOImpl;
+import com.jtechnologies.labs4.models.Student;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.List;
+
+@ManagedBean(name = "StudentBean", eager = false)
+@RequestScoped
+public class StudentBean {
+
+    private int id;
+    private StudentDAO studentsDAO;
+
+    public StudentBean() {
+        studentsDAO = new StudentDAOImpl();
+    }
+
+    public List<Student> getStudents() {
+        return studentsDAO.getStudents();
+    }
+
+    public Student getStudentById() {
+        return studentsDAO.getStudentById(id);
+    }
+
+    public void removeStudentById() {
+        studentsDAO.removeStudentById(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
